96d23cf438d3ec7002670c6fd07dc5ffaeab3c8c
Aakash Sharma <aakashjava04@gmail.com>
2025-04-10 20:33:12 +0530

    Initial commit - P1BookStore Spring Boot project

---
+++ src/main/java/com/book/P1BookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1BookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1BookStoreApplication.class, args);
+	}
+
+}
